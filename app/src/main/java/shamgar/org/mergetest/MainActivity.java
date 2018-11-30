c19356ab42a9c746228886ced6898be64a2c20e5
Praful Nayak <prafulnayak1988@gmail.com>
2018-11-30 17:26:16 +0530

    praful changes

--- app/src/main/java/shamgar/org/mergetest/MainActivity.java
+++ app/src/main/java/shamgar/org/mergetest/MainActivity.java
@@ -2,12 +2,30 @@ package shamgar.org.mergetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textView;
+    Button buttonX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.text);
+        buttonX = findViewById(R.id.button);
+        buttonX.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Hi");
+            }
+        });
     }
 }
